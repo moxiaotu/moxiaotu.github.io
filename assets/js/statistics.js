faea73657e9c26a440068aec8c2b652683a267a8
moxiaotu <164700794@qq.com>
2017-11-04 22:21:10 +0800

    统计

---
+++ assets/js/statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9b1157c473e9f2d530315fd7c96d8cc9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
